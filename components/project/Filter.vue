3f7a4d6c1a0109bb84e5420b540e00597a1bd6de
Jeroen Breen <jeroen@innouveau.nl>
2025-06-10 14:39:39 +0200

    design

--- components/project/Filter.vue
+++ components/project/Filter.vue
@@ -15,9 +15,19 @@ const toggle = (type: string) => {
 
 <template>
     <div class="Filter">
+        <p>
+            Innouveau is een innovatief bureau
+            voor webdevelopment.
+        </p>
+        <p>
+            We schrijven code, maar houden van design.
+        </p>
+
+        <p>
         Wij maken <project-filter-button :filter="filter" type="app" @click="toggle('app')">Online applicaties</project-filter-button>,
         <project-filter-button :filter="filter" type="info" @click="toggle('info')">Interactieve infographics</project-filter-button> en
         <project-filter-button :filter="filter" type="3d" @click="toggle('3d')">3d configuratoren</project-filter-button>.
+        </p>
     </div>
 </template>
 
@@ -25,10 +35,10 @@ const toggle = (type: string) => {
 <style lang="scss" scoped>
 .Filter {
     font-size: 22px;
-    width: 300px;
+    width: 460px;
     max-width: 100%;
     margin: 0 auto 60px auto;
-    font-weight: 700;
     line-height: 1.8;
+    font-weight: 700;
 }
 </style>
