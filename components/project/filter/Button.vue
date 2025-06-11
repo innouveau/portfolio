f310bbb94aed35ae24a0bc78e0d32fc7da133df7
Jeroen Breen <jeroen@innouveau.nl>
2025-06-11 10:27:32 +0200

    design filter buttons

--- components/project/filter/Button.vue
+++ components/project/filter/Button.vue
@@ -17,7 +17,11 @@ const isActive = computed(() => {
 
 <template>
     <button class="Button" :aria-pressed="isActive" >
-        <slot />
+        <span class="Button__check" />
+        <span class="Button__label" >
+            <slot />
+        </span>
+
     </button>
 </template>
 
@@ -28,6 +32,35 @@ const isActive = computed(() => {
     background: #fff;
     border: 1px solid #FFD400;
     font-size: inherit;
+    font-family: 'Dosis', sans-serif;
+    display: inline-flex;
+    align-items: center;
+    padding-right: 6px;
+    margin-right: 2px;
+
+    &__check {
+        width: 16px;
+        height: 16px;
+        background: #fff;
+        border-radius: 50%;
+        margin-right: 4px;
+        transition: background 0.3s ease-in-out;
+        border: 1px solid #c9a800;
+        padding: 2px;
+
+        &:after {
+            content: '';
+            width: 100%;
+            height: 100%;
+            background: #fff;
+            border-radius: 50%;
+            display: block;
+        }
+    }
+
+    &__label {
+        margin-bottom: 2px;
+    }
 
     &:hover {
         border: 1px solid #000;
@@ -40,6 +73,13 @@ const isActive = computed(() => {
     &[aria-pressed="true"] {
         background: #FFD400;
         color: #fff;
+
+        .Button__check {
+
+            &:after {
+                background: #c9a800;
+            }
+        }
     }
 }
 </style>
